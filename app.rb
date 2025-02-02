70ee2affdd61c62bbe13e9d3bb96fc1570989ae5
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-02 02:48:28 +0000

    final sinatra dice roll with form

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/process_roll") do
+  dice = params[:dice].to_i
+  sides = params[:sides].to_i
+
+  @rolls = []
+  dice.times do
+    @rolls <<rand(1..sides)
+  end
+  
+  erb(:flexible)
+
+
 end
